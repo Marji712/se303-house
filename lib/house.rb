42c3238253d17ad4c62637b2711982a35b3ce734
Marji Symonds <symondsm@oregonstate.edu>
2021-04-15 17:39:00 -1000

    Add case for when param is 2

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,9 @@
 class House
     def line(num)
+        if num == 2
+            "This is the malt that lay in the house that Jack built.\n"
+        else
         "This is the house that Jack built.\n"
+        end
     end
 end
