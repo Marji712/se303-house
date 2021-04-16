0126832b340ee5ee28d003cbe0b0b07e792c651b
Marji Symonds <symondsm@oregonstate.edu>
2021-04-15 17:44:23 -1000

    Add verse 7

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,8 @@ class House
     def line(num)
         "This is the " + 
         case num
+        when 7
+            "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 6
             "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 5
