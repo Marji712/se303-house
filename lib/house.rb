e5a9c039fd37e4b5b4b87e052e0019fba392267b
Marji Symonds <symondsm@oregonstate.edu>
2021-04-15 17:40:22 -1000

    Add switch statement for num param

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,9 @@
 class House
     def line(num)
-        if num == 2
+        case num
+        when 3
+            "This is the rat that ate the malt that lay in the house that Jack built.\n"
+        when 2
             "This is the malt that lay in the house that Jack built.\n"
         else
         "This is the house that Jack built.\n"
