c5b71b30eb4aa5694fdac22b71a0d98ceddd77fc
Marji Symonds <symondsm@oregonstate.edu>
2021-05-15 18:31:33 -0700

    Add case 6 to phrase

--- lib/house.rb
+++ lib/house.rb
@@ -35,6 +35,8 @@ class House
             "cat that killed the rat that ate the malt that lay in the "
         when 5
             "dog that worried the cat that killed the rat that ate the malt that lay in the "
+        when 6
+            "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         else
             ""
         end
