40e79423ecafbd4df4ab7628d9a39d50defb1ef7
Marji Symonds <symondsm@oregonstate.edu>
2021-05-20 16:21:03 -0700

    Remove case 4

--- lib/house.rb
+++ lib/house.rb
@@ -9,13 +9,11 @@ class House
 
     def phrase(num)
         verses = [
-            "cat that killed the rat that ate the malt that lay in the",
+            "cat that killed the",
             "rat that ate the",
             "malt that lay in the",
             ""]
         case num
-        when 4
-            "cat that killed the rat that ate the malt that lay in the "
         when 5
             "dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 6
