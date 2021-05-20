07d0b4d620f088d043464f1f2b14ccdcb1be616b
Marji Symonds <symondsm@oregonstate.edu>
2021-05-20 16:11:25 -0700

    Add else case to array

--- lib/house.rb
+++ lib/house.rb
@@ -8,6 +8,7 @@ class House
     end
 
     def phrase(num)
+        verses = [""]
         case num
         when 2
             "malt that lay in the "
@@ -32,9 +33,9 @@ class House
         when 12
             "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         else
-            ""
+            verses.last(num).join(" ")
         end
-        verses = []
+        
     end
 
 end
