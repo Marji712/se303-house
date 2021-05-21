e64f0eed5347f0b65cf58650acbdeb7c44ad600e
Marji Symonds <symondsm@oregonstate.edu>
2021-05-20 17:41:25 -0700

    Remove case statment completely

--- lib/house.rb
+++ lib/house.rb
@@ -9,7 +9,7 @@ class House
 
     def phrase(num)
         verses = [
-            "horse and the hound and the",
+            "horse and the hound and the horn that belonged to the",
             "farmer sowing his corn that kept the",
             "rooster that crowed in the morn that woke the",
             "priest all shaven and shorn that married the",
@@ -21,13 +21,7 @@ class House
             "rat that ate the",
             "malt that lay in the",
             ""]
-        case num
-        when 12
-            "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
-        else
-            verses.last(num).join(" ")
-        end
-        
+        verses.last(num).join(" ")  
     end
 
 end
