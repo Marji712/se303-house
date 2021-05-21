71cb0dd40c530a6c828252b1c794ad450f813435
Marji Symonds <symondsm@oregonstate.edu>
2021-05-20 17:35:45 -0700

    Remove case 6

--- lib/house.rb
+++ lib/house.rb
@@ -16,8 +16,6 @@ class House
             "malt that lay in the",
             ""]
         case num
-        when 6
-            "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 7
             "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 8
