d7552ee6713edfe368e8c8ca96b5581b6a81e6eb
Marji Symonds <symondsm@oregonstate.edu>
2021-05-20 17:37:49 -0700

    Remove case 9

--- lib/house.rb
+++ lib/house.rb
@@ -19,8 +19,6 @@ class House
             "malt that lay in the",
             ""]
         case num
-        when 9
-            "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 10
             "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 11
